c8cc4facd9bba333c3c95cdcb34e01cb96027470
hoslack <hoslackochieng@gmail.com>
2017-12-06 13:18:19 +0200

    corrected getdata from the backend Reference #18

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,28 @@
 function request(url, cb) {
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
-    if (xhr.status === 200 && xhr.responseText === 4) {
-      cb(err, xhr.responseText);
-    } else {
-      cb("error" + xhr.responseText);
+    if (xhr.status === 200 && xhr.readyState === 4) {
+      cb(null, xhr.responseText);
+    } else if(xhr.status === 302 && xhr.readyState === 4){
+       cb(null, xhr.responseText);
+    }else if(xhr.readyState === 4){
+      cb('err' + xhr.responseText);
     }
   };
   xhr.open("GET", url, true);
   xhr.send();
 }
 
-function getData(err, data) {
+function getData(err, ndata) {
   if (err) {
     console.log('GetData Error is :' + err);
   }
-  var parse = JSON.parse(JSON.stringify(data));
-  console.log(parse);
+  // console.log(ndata);
+  var parse = JSON.parse(JSON.stringify(ndata));
   var element = document.createElement('h1');
-  element.innerHTML = parse.name;
-  body.appendChild(element);
+  // console.log('result fron end',parse);
+  element.innerHTML = parse;
+  document.querySelector('#container').appendChild(element);
 }
 
 request('/users', getData);
