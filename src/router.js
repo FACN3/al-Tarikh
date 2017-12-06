f31de05a58ee79aaa37f5291ab1c0060f33db43e
MarlenAwwad <marlenawsh3@gmail.com>
2017-12-06 17:27:37 +0200

    added a query for adding events, up and running, need more modifying
    Related to issue #22

--- src/router.js
+++ src/router.js
@@ -1,60 +1,60 @@
 const handler = require("./handler");
-const qs = require('querystring');
+const qs = require("querystring");
 
 const router = (req, res) => {
   var url = req.url;
   var method = req.method;
   console.log(url);
-  if(url === '/' || url.includes('/public')){
-    handler.publicHandler(req,res);
-  }else if (url === '/users') {
-    handler.getData((err,users)=>{
-      if(err){
-        console.log('router error: ' + err);
-        res.writeHead(500, {"Content-Type": "text/html"});
-        res.end('Erroooooooooor');
-      }else{
-        res.writeHead(200, {"Location":"/" , 'Content-Type':'application/json'});
-        console.log('the users res frob backend ',JSON.stringify(users));
+  if (url === "/" || url.includes("/public")) {
+    handler.publicHandler(req, res);
+  } else if (url === "/users") {
+    handler.getData((err, users) => {
+      if (err) {
+        console.log("router error: " + err);
+        res.writeHead(500, { "Content-Type": "text/html" });
+        res.end("Erroooooooooor");
+      } else {
+        res.writeHead(200, {
+          Location: "/",
+          "Content-Type": "application/json"
+        });
+        console.log("the users res frob backend ", JSON.stringify(users));
 
         res.end(JSON.stringify(users));
       }
     });
-  }else if(url==='/postdata' && method === 'POST' ) {
-    let data = '';
+  } else if (url === "/postdata" && method === "POST") {
+    let data = "";
 
-    req.on('data', (chunk)=>{
-      data +=chunk;
+    req.on("data", chunk => {
+      data += chunk;
     });
 
-    req.on('end', ()=>{
+    req.on("end", () => {
       const parsedData = qs.parse(data);
       console.log(parsedData);
       const name = parsedData.name;
       const title = parsedData.title;
       const description = parsedData.description;
-      const date = parsedData.date;
+      const dt = parsedData.date;
 
-      handler.postData(name, (err, result)=>{
-
-        if(err){
-          res.writeHead(500, {'Content-Type':'text/html'});
+      handler.postData(name, title, description, dt,(err, result) => {
+        if (err) {
+          res.writeHead(500, { "Content-Type": "text/html" });
           res.end(err);
         }
 
-        console.log('User inserted into the database', result);
-        res.writeHead(302, {"Location":"/" , 'Content-Type':'application/json'});
+        console.log("User inserted into the database", result);
+        res.writeHead(302, {
+          Location: "/",
+          "Content-Type": "application/json"
+        });
         res.end();
-
       });
-
     });
-
-  }
-  else{
+  } else {
     res.end("errorrrrr");
-
   }
-}
+};
 
 module.exports = router;
